1a2df60f50589319b454b0e38764167fa47a675b
henriquelbk <loubackhenrique123@gmail.com>
2023-08-28 22:09:52 -0300

    quase

---
+++ exercicios/process-argv.js
@@ -0,0 +1,7 @@
+console.log("whatever");
+
+const meuArgumento1 =  process.argv[2];
+const meuArgumento2 =  process.argv[3];
+
+console.log(meuArgumento1);
+console.log(meuArgumento2);
---
+++ index.js
@@ -0,0 +1,1 @@
+console.log("O app foi iniciado");
